07458bd8802c727e983f094258a8263bcafca525
UlysseRis <122040075+UlysseRis@users.noreply.github.com>
2026-09-11 19:06:42 +0200

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        left = 0
+        freq = Counter(s1)
+        n = len(s1)
+        if n > len(s2):
+            return False
+        for right, char in enumerate(s2):
+            freq[char] = freq.get(char, 0) - 1
+            while freq[char] < 0:
+                freq[s2[left]] += 1
+                left += 1
+            if (right - left + 1 == n):
+                return True
+        return False
+        
